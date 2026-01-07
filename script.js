fcb17784c412f23a666e4b5e572c3d2a543ff231
KenjiRaizo <osciarzyna20@gmail.com>
2026-01-07 19:56:41 +0100

    script.js

--- script.js
+++ script.js
@@ -11,18 +11,21 @@ function drawCalendar(){
     const firstDay = new Date(y,m,1).getDay() || 7;
     const days = new Date(y,m+1,0).getDate();
     let row = cal.insertRow();
-    ["P","W","Ś","C","P","S","N"].forEach(x=>{let c=row.insertCell(); c.innerHTML="<b>"+x+"</b>";});
+    ["P","W","Ś","C","P","S","N"].forEach(x=>{
+        let c = row.insertCell();
+        c.innerHTML = "<b>"+x+"</b>";
+    });
     row = cal.insertRow();
     for(let i=1;i<firstDay;i++) row.insertCell();
     for(let i=1;i<=days;i++){
         if(row.cells.length===7) row = cal.insertRow();
-        let c = row.insertCell();
-        c.textContent = i;
+        let cell = row.insertCell();
+        cell.textContent = i;
         let t = new Date();
-        if(i===t.getDate() && m===t.getMonth() && y===t.getFullYear()) c.className="today";
+        if(i===t.getDate() && m===t.getMonth() && y===t.getFullYear()) cell.className="today";
     }
 }
 
-prevBtn.addEventListener("click",()=>{d.setMonth(d.getMonth()-1);drawCalendar();});
-nextBtn.addEventListener("click",()=>{d.setMonth(d.getMonth()+1);drawCalendar();});
+prevBtn.addEventListener("click", ()=>{ d.setMonth(d.getMonth()-1); drawCalendar(); });
+nextBtn.addEventListener("click", ()=>{ d.setMonth(d.getMonth()+1); drawCalendar(); });
 drawCalendar();
